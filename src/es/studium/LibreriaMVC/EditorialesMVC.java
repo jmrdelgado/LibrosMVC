b3a7783d41c934edf78570dd7e352cc70cec3f4c
Jose Manuel Rufo <jmrufo@ideoformacion.com>
2019-05-02 13:47:19 +0200

    Inicio CRUD Editoriales

---
+++ src/es/studium/LibreriaMVC/EditorialesMVC.java
@@ -0,0 +1,19 @@
+package es.studium.LibreriaMVC;
+
+/**
+*
+* @author Jose Manuel Rufo Delgado
+* EditorialesMVC
+* Encapsula la comunicación con la base de datos
+* Almacena, Edita y Elimina nombre de la Editorial
+*
+*/
+
+public class EditorialesMVC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
